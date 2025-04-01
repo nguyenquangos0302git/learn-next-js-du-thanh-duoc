3d237a098b7f3b0a3420324ffeb2edf8c339d09e
Nguyen Quang <nguyenquang@Nguyens-MacBook-Pro-5.local>
2025-04-01 22:30:31 +0700

    image

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h3 className="font-sans font-bold">text  </h3>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        {/* <Image
+          src={'/next.svg'}
+          alt="test"
+          width={500}
+          height={500}
+        /> */}
+        <Image
+          src={'https://images.pexels.com/photos/31237847/pexels-photo-31237847/free-photo-of-vintage-tea-setting-with-book-and-flowers.jpeg'}
+          alt="test"
+          width={200}
+          height={200}
+          quality={75}
+        />
+      </div>
     </div>
   );
 }
